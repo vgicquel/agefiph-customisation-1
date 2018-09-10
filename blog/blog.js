1351b4b22ace9e2ef7f60d5da75db52cd2d4bee6
Vincent GICQUEL <37332811+vgicquel@users.noreply.github.com>
2018-09-10 12:48:09 +0200

    Update blog.js

--- blog/blog.js
+++ blog/blog.js
@@ -5,7 +5,26 @@ BlogJS.do(function () {
 		console.debug(dojo.query("div[style='border-top: 2px solid black;height:45px;']"));
 
 		/* Suppression de la ligne noire */
-		dojo.query("div[style='border-top: 2px solid black;height:45px;']").style("border-top","0px solid black")
+		dojo.query("div[style='border-top: 2px solid black;height:45px;']").style("border-top","0px solid black");
+	
+		/* Recherche de la balise */    
+		console.debug(dojo.query("ul[class='inlinelist lotusInlinelist']"));
+	
+		var list = document.getElementsByClassName("inlinelist lotusInlinelist")[0].getElementsByTagName("LI");
+	
+		for (n=0; n <= dojo.query("ul[class='inlinelist lotusInlinelist']").length; n++) {
+			  dojo.place("<div class='lotusActionBar lotusBtnContainer aria_toolbar' role='toolbar' style='float:none' aria-label='Actions de blogue'>" + 
+				 "<span class='lotusBtn lotusLeft'>" +
+				 list[0].innerHTML + 
+				 "</span>" +
+				 "<span class='lotusBtn lotusLeft'>" +
+				 list[1].innerHTML +
+				 "</span>" +
+				 "<span class='lotusBtn lotusLeft'>" +
+				 list[2].innerHTML +
+				 "</span>" +
+				 "</div>", dojo.query(".inlinelist")[n],"replace");
+		}
 	
 	}
 	  
